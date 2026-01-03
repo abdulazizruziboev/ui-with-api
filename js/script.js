857edd8202b8ba1256fd2f2a0c9eb9e5d7816b77
abdulaziz <telegrampreimum4@gmail.com>
2026-01-03 09:06:14 +0500

    Funtions logic changed

--- js/script.js
+++ js/script.js
@@ -5,74 +5,69 @@ let elementsArray = [];
 
 fetch("https://json-api.uz/api/project/fn44-amaliyot/cars")
 .then((res)=>{
-    return res.json();
+return res.json();
 }
 ).then((res)=>
 {
-    res.data.forEach(el=>elementsArray.push(el));
+res.data.forEach(el=>elementsArray.push(el));
 }).finally(()=>{
 setTimeout(()=>document.getElementById("loader").style.display="none",5);
-uiWrite(elementsArray);
+uiWrite(elementsArray)
 });
 
-function uiWrite(data){
-data.forEach((el) => {
+function uiWrite(arr) {
+elBox.innerHTML='';
+if(arr!='') {
+arr.forEach((el)=>{
 let elHTMLStucture = 
 `<div class="card bg-base-100 w-96 lg:w-115 shadow-sm bg-[#f4f4f4] border-1 border-[#0002]">
-    <figure>
-        <img
-        src="${el.image}"
-        alt="${el.name}" class="bg-[#f1f1f1] h-[285px] w-full border-[#0001] border-b-1" />
-    </figure>
-    <div class="card-body">
-        <h2 class="card-title">
-        ${el.name}
-        </h2>
-        <div>
-        <p>Fuel: <span class="font-bold">${el.details.fuel}</span class="font-bold"></p>
-        <p>Gearbox: <span class="font-bold">${el.details.gearbox}</span class="font-bold"></p>
-        <p>Drive: <span class="font-bold">${el.drive}</span class="font-bold"></p>
-        <p>Seats: <span class="font-bold">${el.details.seats}</span class="font-bold"></p>
-        </div>
-        <div class="card-actions justify-end">
-        <div class="badge badge-outline">${el.type}</div>
-        </div>
-    </div>
+<figure>
+<img
+src="${el.image}"
+alt="${el.name}" class="bg-[#f1f1f1] h-[285px] w-full border-[#0001] border-b-1" />
+</figure>
+<div class="card-body">
+<h2 class="card-title">
+${el.name}
+</h2>
+<div>
+<p>Fuel: <span class="font-bold">${el.details.fuel}</span class="font-bold"></p>
+<p>Gearbox: <span class="font-bold">${el.details.gearbox}</span class="font-bold"></p>
+<p>Drive: <span class="font-bold">${el.drive}</span class="font-bold"></p>
+<p>Seats: <span class="font-bold">${el.details.seats}</span class="font-bold"></p>
+</div>
+<div class="card-actions justify-end">
+<div class="badge badge-outline">${el.type}</div>
+</div>
+</div>
 </div>`;
-setTimeout(()=>elBox.innerHTML+=elHTMLStucture,10)
+elBox.innerHTML+=elHTMLStucture;
 });
+}
+
 };
 
-function find(val){
+function uiFindWrite(val) {
     let arr = [];
     elementsArray.filter((el)=>{
-        if(el.name.trim().toLowerCase().includes(val.trim().toLowerCase())==true) arr.push(el);
-        else {    
-        return false;
-        }; 
+        if(el.name.toLowerCase().includes(val.trim().toLowerCase())==true) return arr.push(el);
     });
     uiWrite(arr);
-    console.log(arr);
 };
 
 elSearch.addEventListener("input",(evt)=>{
-    elBox.innerHTML=null;
-    find(evt.target.value);
-});
+    uiFindWrite(evt.target.value);
+})
 
-function filterCars(type){
+function uiFilteredWrite(val) {
     let arr = [];
-    elementsArray.filter(el=>{
-        if(type=="no") {
-            arr=elementsArray;
-        }
-        else if(type.toLowerCase()==el.type.toLowerCase()) {return arr.push(el)};
-    })
-    elBox.innerHTML=null;
-    uiWrite(arr)
-    return arr;
+    elementsArray.filter((el)=>{
+        if(el.type.toLowerCase()==val.toLowerCase()) return arr.push(el);
+    });
+    uiWrite(arr);
+    if(val.toLowerCase()=="no") uiWrite(elementsArray);
 }
 
 elFilter.addEventListener("change",(evt)=>{
-    filterCars(evt.target.value);
+    uiFilteredWrite(evt.target.value);
 });
